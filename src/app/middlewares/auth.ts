a56f25a54db373d07413ce5132fc37dfaea9c4a3
arafat-sabbir <arafatshabbir8@gmail.com>
2024-06-13 20:54:23 +0600

    Implemented Authorization on Service.create

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -1,24 +1,27 @@
-import { NextFunction, Request, Response } from 'express';
-import catchAsync from '../utils/catchAsync';
-import AppError from '../errors/AppError';
-import jwt, { JwtPayload } from 'jsonwebtoken';
-import config from '../config';
+import { NextFunction, Request, Response } from "express";
+import { TUserRole } from "../modules/user/user.interface";
+import catchAsync from "../utils/catchAsync";
+import AppError from "../errors/AppError";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import config from "../config";
 
-const AuthorizeRequest = () => {
+
+const AuthorizeRequest = (...requiredRoles: TUserRole[]) => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     // Get the authorization token from the request headers
-    const token = req.headers.authorization?.split(' ')[1];
+    const token = req.headers.authorization?.split(" ")[1];
     // If no token is provided, throw an unauthorized error
     if (!token) {
-      throw new AppError(401, 'Unauthorized Access');
+      throw new AppError(401, "Unauthorized Access");
     }
-    jwt.verify(token, config.jwt_access_secret as string, (err: any, decoded: any) => {
-      if (err) {
-        throw new AppError(401, 'Unauthorized Access');
-      }
-      req.user = decoded as JwtPayload;
-      next();
-    });
+    const decoded = jwt.verify(token, config.jwt_access_secret as string);
+    const { role } = decoded as JwtPayload;
+    if (!decoded) throw new AppError(401, "Unauthorized Access");
+    if (requiredRoles && !requiredRoles.includes(role)) {
+      throw new AppError(401, "You have no access to this route!");
+    }
+    req.user = decoded as JwtPayload;
+    next();
   });
 };
 
--- src/app/modules/service/service.route.ts
+++ src/app/modules/service/service.route.ts
@@ -2,10 +2,13 @@ import express from "express";
 import validateRequest from "../../middlewares/validateRequest";
 import { serviceValidation } from "./service.validation";
 import { serviceController } from "./service.controller";
+import AuthorizeRequest from "../../middlewares/auth";
+import { USER_ROLE } from "../user/user.constant";
 const router = express.Router();
 
 router.post(
   "/",
+  AuthorizeRequest(USER_ROLE.admin),
   validateRequest(serviceValidation.createServiceValidationSchema),
   serviceController.create
 );
