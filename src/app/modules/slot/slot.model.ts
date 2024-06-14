7aa4429932b3472645c7aebed87bf20b43910da9
arafat-sabbir <arafatshabbir8@gmail.com>
2024-06-14 10:04:37 +0600

    Added Timestamp To SlotModel ANd ServiceModel

--- src/app/modules/slot/slot.model.ts
+++ src/app/modules/slot/slot.model.ts
@@ -1,17 +1,20 @@
 import { Schema, model } from "mongoose";
 import { TSlot } from "./slot.interface";
 
-const slotSchema = new Schema<TSlot>({
-  service: { type: Schema.Types.ObjectId, ref: "Service", required: true },
-  date: { type: Date, required: true },
-  startTime: { type: String, required: true },
-  endTime: { type: String, required: true },
-  isBooked: {
-    type: String,
-    enum: ["available", "booked", "canceled"],
-    default: "available",
+const slotSchema = new Schema<TSlot>(
+  {
+    service: { type: Schema.Types.ObjectId, ref: "Service", required: true },
+    date: { type: Date, required: true },
+    startTime: { type: String, required: true },
+    endTime: { type: String, required: true },
+    isBooked: {
+      type: String,
+      enum: ["available", "booked", "canceled"],
+      default: "available",
+    },
   },
-});
+  { timestamps: true }
+);
 
 const SlotModel = model<TSlot>("Slot", slotSchema);
 export default SlotModel;
